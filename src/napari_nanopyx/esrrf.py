8f9aadd09f51b3b6084340a30746035121e09e8a
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2023-08-11 16:10:14 +0100

    updated to use LE. added eSRRF

---
+++ src/napari_nanopyx/esrrf.py
@@ -0,0 +1,80 @@
+import numpy as np
+from nanopyx.methods import eSRRF_w
+from nanopyx.core.transform.sr_temporal_correlations import calculate_eSRRF_temporal_correlations
+from napari.layers import Image
+from napari import Viewer
+from magicgui import magic_factory
+
+
+@magic_factory(
+    call_button="Generate",
+    img={"label": "Image Stack"},
+    magnification={
+        "value": 5,
+        "label": "Magnification",
+        "min": 1,
+        "max": 10},
+    sensitivity={
+        "value": 1,
+        "label": "Sensitivity",
+        "min": 1,
+        "max": 10},
+    radius={
+        "value": 1.5,
+        "label": "Ring Radius",
+        "min": 0.1,
+        "max": 3.0},
+    frames_per_timepoint={
+        "value": 0,
+        "label": "Frames Per Time Point (0 = auto)",
+        "min": 0,
+        "max": 100000},
+    do_intensity_weighting={
+        "value": True,
+        "label": "Apply Intensity Weighting"},
+    reconstruction_order={
+        "widget_type": "RadioButtons",
+        "orientation": "vertical",
+        "value": "AVG",
+        "choices": [("Average", "AVG"), ("Variance", "VAR"), ("Autocorrelation", "TAC2")],
+        "label": "Shift Calculation Method"}
+    )
+def generate_esrrf_image(
+        viewer: Viewer,
+        img: Image, magnification: int,
+        sensitivity: float,
+        radius: float,
+        frames_per_timepoint: int,
+        do_intensity_weighting: bool,
+        reconstruction_order: str
+        ):
+    
+    if frames_per_timepoint == 0:
+        frames_per_timepoint = img.data.shape[0]
+    elif frames_per_timepoint > img.data.shape[0]:
+        frames_per_timepoint = img.data.shape[0]
+
+    output_stack = []
+
+    for i in range(img.data.shape[0] // frames_per_timepoint):
+        block = img.data[i*frames_per_timepoint:(i+1)*frames_per_timepoint]
+        eSRRF_generator = eSRRF_w(
+            block,
+            magnification=magnification,
+            radius=radius,
+            sensitivity=sensitivity,
+            doIntensityWeighting=do_intensity_weighting
+            )
+
+        output_stack.append(calculate_eSRRF_temporal_correlations(eSRRF_generator.calculate()[0], reconstruction_order))
+
+    output_stack = np.array(output_stack)
+
+    if output_stack is not None:
+        result_esrrf_name = img.name + "_esrrf"
+        try:
+            # if the layer exists, update the data
+            viewer.layers[result_esrrf_name].data = output_stack
+        except KeyError:
+            # otherwise add it to the viewer
+            viewer.add_image(output_stack, name=result_esrrf_name)
--- src/napari_nanopyx/srrf.py
+++ src/napari_nanopyx/srrf.py
@@ -1,52 +1,67 @@
-from nanopyx.methods.srrf import SRRF
+import numpy as np
+from nanopyx.methods.SRRF_workflow import SRRF
+from nanopyx.core.transform.sr_temporal_correlations import calculate_SRRF_temporal_correlations
 from napari.layers import Image
 from napari import Viewer
 from magicgui import magic_factory
 
+@magic_factory(
+    call_button="Generate",
+    img={"label": "Image Stack"},
+    magnification={
+        "value": 4,
+        "label": "Magnification",
+        "min": 1,
+        "max": 10},
+    ring_radius={
+        "value": 0.5,
+        "label": "Ring Radius",
+        "min": 0.1,
+        "max": 3.0},
+    frames_per_timepoint={
+        "value": 0,
+        "label": "Frames Per Time Point (0 = auto)",
+        "min": 0,
+        "max": 100000},
+    radiality_positivity_constraint={
+        "value": True,
+        "label": "Radiality Positivity Constraint"},
+    do_intensity_weighting={
+        "value": True,
+        "label": "Apply Intensity Weighting"},
+    reconstruction_order={
+        "label": "Reconstruction:",
+        "widget_type": "RadioButtons",
+        "orientation": "vertical",
+        "value": 1,
+        "choices": [("Sum", -1), ("Maximum", 0), ("Mean", 1), ("Autocorrelation Order 2", 2), ("Autocorrelation Order 3", 3), ("Autocorrelation Order 4", 4)],
+    })
+def generate_srrf_image(viewer: Viewer, img: Image, frames_per_timepoint: int, magnification: int, ring_radius: float, radiality_positivity_constraint: bool, do_intensity_weighting: bool, reconstruction_order: int):
+    
+    if frames_per_timepoint == 0:
+        frames_per_timepoint = img.data.shape[0]
+    elif frames_per_timepoint > img.data.shape[0]:
+        frames_per_timepoint = img.data.shape[0]
 
-@magic_factory(call_button="Generate",
-               img={"label": "Image Stack"},
-               magnification={"value": 4,
-                              "label": "Magnification",
-                              "min": 1,
-                              "max": 10},
-               ring_radius={"value": 0.5,
-                            "label": "Ring Radius",
-                            "min": 0.1,
-                            "max": 3.0},
-               frames_per_timepoint={"value": 0,
-                                     "label": "Frames Per Time Point (0 = auto)",
-                                     "min": 0,
-                                     "max": 100000},
-               radiality_positivity_constraint={"value": True,
-                                                "label": "Radiality Positivity Constraint"},
-               do_intensity_weighting={"value": True,
-                                       "label": "Apply Intensity Weighting"})
-def generate_srrf_image(viewer: Viewer, img: Image, frames_per_timepoint: int, magnification: int, ring_radius: float,
-                        radiality_positivity_constraint: bool, do_intensity_weighting: bool):
+    output= []
+
+    for i in range(img.data.shape[0] // frames_per_timepoint):
+        block = img.data[i*frames_per_timepoint:(i+1)*frames_per_timepoint]
+        srrf_generator = SRRF(block, magnification=magnification, ringRadius=ring_radius,
+                                radialityPositivityConstraint=radiality_positivity_constraint,
+                                doIntensityWeighting=do_intensity_weighting)
+
+        result = srrf_generator.calculate()
+        output.append(calculate_SRRF_temporal_correlations(result[0], reconstruction_order))
+
+    output = np.array(output)
     
-    srrf_generator = SRRF(magnification=magnification, ringRadius=ring_radius,
-                          radialityPositivityConstraint=radiality_positivity_constraint,
-                          doIntensityWeighting=do_intensity_weighting)
-    
-    result = srrf_generator.calculate(img.data, frames_per_timepoint)
-    data_srrf = result[0]
-    data_intensity = result[1]
-    
-    if data_srrf is not None:
-        result_srrf_name = img.name + "_srrf"
+    if output is not None:
+        result_output_name = img.name + "_srrf"
         try:
             # if the layer exists, update the data
-            viewer.layers[result_srrf_name].data = data_srrf
+            viewer.layers[result_output_name].data = output
         except KeyError:
             # otherwise add it to the viewer
-            viewer.add_image(data_srrf, name=result_srrf_name)
-    if data_intensity is not None:
-        result_intensity_name = img.name + "_intensity"
-        try:
-            # if the layer exists, update the data
-            viewer.layers[result_intensity_name].data = data_intensity
-        except KeyError:
-            # otherwise add it to the viewer
-            viewer.add_image(data_intensity, name=result_intensity_name)
+            viewer.add_image(output, name=result_output_name)
 
